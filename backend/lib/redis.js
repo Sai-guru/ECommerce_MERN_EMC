7eb34dadeccc9ba7809805d07f156fea41cfe271
Sai-guru <lpnkaveri@gmail.com>
2025-05-01 16:48:03 +0530

    first commit

---
+++ backend/lib/redis.js
@@ -0,0 +1,18 @@
+import Redis from "ioredis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+
+export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+// console.log("Redis URL:", process.env.UPSTASH_REDIS_URL);
+
+
+redis.on('connect', () => {
+  console.log('Redis connected successfully!');
+});
+
+redis.on('error', (err) => {
+  console.error('Redis error:', err);
+});
