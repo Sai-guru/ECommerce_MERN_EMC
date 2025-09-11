2c760a8d8ce7fa4470ac4f4e14cf448fa5e26990
Sai-guru <lpnkaveri@gmail.com>
2025-09-11 09:23:30 +0530

    fix: connected Mongo, Upstash Redis, and Stripe test mode integration

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -5,9 +5,14 @@ dotenv.config();
 
 
 
-export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+// export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
 // console.log("Redis URL:", process.env.UPSTASH_REDIS_URL);
 
+
+export const redis = new Redis(process.env.UPSTASH_REDIS_URL, {
+  tls: {},                 // 👈 enable TLS
+  maxRetriesPerRequest: null // 👈 avoid 20 retry limit crash
+});
 
 redis.on('connect', () => {
   console.log('Redis connected successfully!');
@@ -16,3 +21,6 @@ redis.on('connect', () => {
 redis.on('error', (err) => {
   console.error('Redis error:', err);
 });
+
+
+
